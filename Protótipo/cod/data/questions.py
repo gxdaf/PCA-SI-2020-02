2783d88dbc1c2e0c709a0e420f14bb18a3503a5b
MarlonVictor <cmarlonvictor11@gmail.com>
2020-09-04 11:39:17 -0300

    Quiz com 10 perguntas

---
+++ Protótipo/cod/data/questions.py
@@ -0,0 +1,112 @@
+Data = {
+    "1": {
+        "question": "Estacionar sobre a calçada é:",
+        "options": {
+            "A": "Permitido à noite",
+            "B": "Permitido na frente de escolas",
+            "C": "Uma infração de trânsito",
+            "D": "Sempre permitido"
+        },
+        "answer": "C",
+        "difficulty": "Fácil"
+    },
+    "2": {
+        "question": "Ao se aproximar de um cruzamento com sinal vermelho é correto:",
+        "options": {
+            "A": "Aumentar a velocidade do veículo e passar",
+            "B": "Diminuir a velocidade e parar o veículo",
+            "C": "Diminuir a velocidade do veículo e passar",
+            "D": "Frear bruscamente o veículo"
+        },
+        "answer": "B",
+        "difficulty": "Fácil"
+    },
+    "3": {
+        "question": "Todo condutor, antes de mudar de direção, deverá:",
+        "options": {
+            "A": "Piscar os faróis",
+            "B": "Dar um toque rápido na buzina",
+            "C": "Acender os faróis altos",
+            "D": "Ligar os dispositivos indicadores de direção"
+        },
+        "answer": "D",
+        "difficulty": "Médio"
+    },
+    "4": {
+        "question": "Quando tiver duas faixas continuas amarelas, significa que:",
+        "options": {
+            "A": "Não permite ultrapassagem",
+            "B": "A ultrapassagem permitida",
+            "C": "Ultrapassagem permitida, somente em retas",
+            "D": "Permite a ultrapassagem para um único sentido"
+        },
+        "answer": "A",
+        "difficulty": "Difícil"
+    },
+    "5": {
+        "question": "São considerados crimes de trânsito:",
+        "options": {
+            "A": "Transitar na contramão de direção",
+            "B": "Dirigir sem habilitação",
+            "C": "Transitar em velocidade superior à máxima permitida em mais de 50%.",
+            "D": "Todas as opções a cima"
+        },
+        "answer": "D",
+        "difficulty": "Fácil"
+    },
+    "6": {
+        "question": "Para o condutor ter um comportamento seguro no trânsito ele deve:",
+        "options": {
+            "A": "Dirigir sem atenção",
+            "B": "Dirigir com a velocidade de 65 km por hora",
+            "C": "Perceber antecipadamente os riscos e agir prontamente",
+            "D": "Não dirigir nos feriados e finais de semana"
+        },
+        "answer": "C",
+        "difficulty": "Médio"
+    },
+    "7": {
+        "question": "Quando for realizar uma parada de emergência o condutor deverá:",
+        "options": {
+            "A": "Parar sobre a via",
+            "B": "Sinalizar com o pisca-alerta e o triângulo",
+            "C": "Chamar a assistência técnica",
+            "D": "Não ligar o pisca-alerta"
+        },
+        "answer": "B",
+        "difficulty": "Médio"
+    },
+    "8": {
+        "question": "Ao perceber que será ultrapassado, o condutor deve:",
+        "options": {
+            "A": "Sair para o acostamento",
+            "B": "Parar o veículo",
+            "C": "Manter ou reduzir a velocidade facilitando a ultrapassagem",
+            "D": "Acelerar o veículo"
+        },
+        "answer": "C",
+        "difficulty": "Médio"
+    },
+    "9": {
+        "question": "Em que situações devemos usar a buzina?",
+        "options": {
+            "A": "Em situações de simples toque breve como advertência afim de evitar acidentes",
+            "B": "Após às 4 horas da manhã",
+            "C": "Em situações de engarrafamentos",
+            "D": "Perto de hospitais"
+        },
+        "answer": "A",
+        "difficulty": "Médio"
+    },
+    "10": {
+        "question": "A educação para o trânsito é:",
+        "options": {
+            "A": "Para quem é motorista",
+            "B": "É direito só dos agentes de trânsito",
+            "C": "É direito só para quem é alfabetizado",
+            "D": "É direito de todos"
+        },
+        "answer": "D",
+        "difficulty": "Médio"
+    }
+}
