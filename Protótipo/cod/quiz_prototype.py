ac00699aae1d50f48c1a9a374e9baf2c969a3d05
unknown <gabrielaxavierfontes@unigranrio.br>
2020-10-05 10:22:37 -0300

    sorteio e pontos dos jogadores no protótipo do quiz

--- Protótipo/cod/quiz_prototype.py
+++ Protótipo/cod/quiz_prototype.py
@@ -1,10 +1,10 @@
-import pygame
+import pygame, random
 from questions import Data
 
 pygame.init()
 pygame.font.init()
 
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((600, 600))
 name = pygame.display.set_caption(('City Driving Guide'))
 runing = True
 
@@ -13,30 +13,32 @@ y = [70, 130, 190, 250]
 largura = 500
 altura = 50
 
-#Só mudar o valor de "case" para escolher a pergunta
-case = 1
+# Só mudar o valor de "case" para escolher a pergunta
+case = 0
 choice = ''
-points = 0
-
+points_am = 0
+points_az = 0
+jogadores = ['Amarelo', 'Azul']
 ###########################################################################################
 
 def show_text(txt, x, y, width, color):
     sys_font = pygame.font.SysFont('None', width)
     text = sys_font.render(txt, True, color)
     screen.blit(text, (x, y))
-  
+
+
 def ask(number):
     for key, value in Data.items():
-        #Informa a chave da pergunta e o seu valor
+        # Informa a chave da pergunta e o seu valor
         if key == number:
-            question = '{0}: {1}'.format(key ,value['question'])
-            show_text(question, 10, 10, 20, (0,0,0))
+            question = '{0}: {1}'.format(key, value['question'])
+            show_text(question, 10, 10, 20, (0, 0, 0))
 
-            #Show options
-            show_text(('[A]: {}'.format(value["options"]["A"])), (x + 10), (y[0] + 18), 20, (255,255,255))
-            show_text(('[B]: {}'.format(value["options"]["B"])), (x + 10), (y[1] + 18), 20, (255,255,255))
-            show_text(('[C]: {}'.format(value["options"]["C"])), (x + 10), (y[2] + 18), 20, (255,255,255))
-            show_text(('[D]: {}'.format(value["options"]["D"])), (x + 10), (y[3] + 18), 20, (255,255,255))
+            # Show options
+            show_text(('[A]: {}'.format(value["options"]["A"])), (x + 10), (y[0] + 18), 20, (255, 255, 255))
+            show_text(('[B]: {}'.format(value["options"]["B"])), (x + 10), (y[1] + 18), 20, (255, 255, 255))
+            show_text(('[C]: {}'.format(value["options"]["C"])), (x + 10), (y[2] + 18), 20, (255, 255, 255))
+            show_text(('[D]: {}'.format(value["options"]["D"])), (x + 10), (y[3] + 18), 20, (255, 255, 255))
 
             return value
 
@@ -44,59 +46,84 @@ def ask(number):
 ###########################################################################################
 
 while runing:
-    screen.fill((255,255,255))
-
-    #Desenhando os botões
+    screen.fill((255, 255, 255))
+    escolha = random.choice(jogadores)
+    # Desenhando os botões
     pygame.draw.rect(screen, (50, 137, 168), (x, y[0], largura, altura))
     pygame.draw.rect(screen, (50, 137, 168), (x, y[1], largura, altura))
     pygame.draw.rect(screen, (50, 137, 168), (x, y[2], largura, altura))
     pygame.draw.rect(screen, (50, 137, 168), (x, y[3], largura, altura))
-
     for event in pygame.event.get():
-        #Quit game
+        # Quit game
         if event.type == pygame.QUIT:
             runing = False
 
-        #Evento de Click
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and escolha == 'Amarelo':
+                dado = random.randint(1, 6)
+                case += dado
+                pygame.display.update()
+            elif event.key == pygame.K_SPACE and escolha == 'Azul':
+                dado = random.randint(1, 6)
+                case += dado
+                pygame.display.flip()
+        # Evento de Click
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse = pygame.mouse.get_pos()
-
-            #Posição dos botões
-            if(x < mouse[0] < x + largura and y[0] < mouse[1] < y[0] + altura):
+            # Posição dos botões
+            if (x < mouse[0] < x + largura and y[0] < mouse[1] < y[0] + altura):
                 choice = 'A'
-            elif(x < mouse[0] < x + largura and y[1] < mouse[1] < y[1] + altura):
+            elif (x < mouse[0] < x + largura and y[1] < mouse[1] < y[1] + altura):
                 choice = 'B'
-            elif(x < mouse[0] < x + largura and y[2] < mouse[1] < y[2] + altura):
+            elif (x < mouse[0] < x + largura and y[2] < mouse[1] < y[2] + altura):
                 choice = 'C'
-            elif(x < mouse[0] < x + largura and y[3] < mouse[1] < y[3] + altura):
+            elif (x < mouse[0] < x + largura and y[3] < mouse[1] < y[3] + altura):
                 choice = 'D'
 
-            #Pegando os valores da função
+            # Pegando os valores da função
             ask_values = ask(case)
-            answer = ask_values["answer"]#Resposta
-            difficulty = ask_values["difficulty"]#Dificuldade
+            answer = ask_values["answer"]  # Resposta
+            difficulty = ask_values["difficulty"]  # Dificuldade
 
-            #Conferindo a resposta
-            if choice == answer:
-                print("Certa resposta!")
-                case += 1
-            else:
-                if difficulty == "Fácil":
-                    points += 3
-                    case += 1
-                elif difficulty == "Médio":
-                    points += 2
-                    case += 1
-                elif difficulty == "Difícil":
-                    points += 1
-                    case += 1
-                print("Você errou, a resposta correta era [{}]".format(answer))
-
-
-    #Placar de pontos
-    show_points = "Pontos na carteira: " + str(points)
-    show_text(show_points, 10, 580, 25, (0,0,0))
-
-    #Chamando a função e atualizando o display a cada lopping
+            # Conferindo a resposta
+            if escolha == 'Amarelo':
+                if choice == answer:
+                    print("Certa resposta! Amarelo joga novamente")
+                    escolha = escolha
+                else:
+                    if difficulty == "Fácil":
+                        points_am += 3
+                        escolha = 'Azul'
+                    elif difficulty == "Médio":
+                        points_am += 2
+                        escolha = 'Azul'
+                    elif difficulty == "Difícil":
+                        points_am += 1
+                        escolha = 'Azul'
+                    print("Você errou, a resposta correta era [{}]".format(answer))
+                    print('Vez do Azul! Por favor, jogue o dado.')
+            if escolha == 'Azul':
+                if choice == answer:
+                    print("Certa resposta! Azul joga novamente")
+                    escolha = escolha
+                else:
+                    if difficulty == "Fácil":
+                        points_az += 3
+                        escolha = 'Amarelo'
+                    elif difficulty == "Médio":
+                        points_az += 2
+                        escolha = 'Amarelo'
+                    elif difficulty == "Difícil":
+                        points_az += 1
+                        escolha = 'Amarelo'
+                    print("Você errou, a resposta correta era [{}]".format(answer))
+                    print('Vez do Amarelo! Por favor, jogue o dado.')
+    # Placar de pontos
+    show_points_am = "Carteira amarela: " + str(points_am)
+    show_points_az = "Carteira azul: " + str(points_az)
+    show_text(show_points_am, 10, 580, 25, (0, 0, 0))
+    show_text(show_points_az, 460, 580, 25, (0, 0, 0))
+    # Chamando a função e atualizando o display a cada lopping
     ask(case)
     pygame.display.update()
+
