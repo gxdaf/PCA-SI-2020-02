e45b48cd0fa265b15f08062d473135a5d5799fa3
unknown <gabrielaxavierfontes@unigranrio.br>
2020-10-12 18:13:13 -0300

    questoes adulto

---
+++ Protótipo/cod/questions_adulto.py
@@ -0,0 +1,253 @@
+Data = {
+    1: {
+        "status": "Buraco"
+    },
+
+    2: {
+        "question": "Conduzir o veículo sem os documentos de porte obrigatórios acarretará em:",
+        "options": {
+            "A": "Multa e remoção do veículo.",
+            "B": "Multa e retenção do veículo para regularização.",
+            "C": "Multa e apreensão da CNH.",
+            "D": "Multa apenas."
+        },
+        "answer": "B",
+        "difficulty": "Fácil"
+    },
+    3: {
+        "question": "Diante da placa de regulamentação R-6c, o condutor poderá:": {
+            "A": "Estacionar apenas para carga e descarga.",
+            "B": "Estacionar seu veículo.",
+            "C": "Parar apenas para embarque e desembarque.",
+            "D": "Ser multado se parar e estacionar."
+        },
+        "answer": "D",
+        "difficulty": "Médio"
+    },
+    4: {
+        "question": "Um condutor deseja ultrapassar um ônibus que está parado, efetuando embarque e desembarque. A conduta adequada do condutor, ante essa situação, deve ser:",
+        "options": {
+            "A": "Manter a velocidade e buzinar com intensidade para alertar os pedestres.",
+            "B": "Reduzir a velocidade e piscar os faróis para alertar os pedestres.",
+            "C": "Reduzir a velocidade e ficar atento a algum movimento dos pedestres.",
+            "D": "Manter a velocidade e buzinar para alertar os pedestres."
+        },
+        "answer": "C",
+        "difficulty": "Médio"
+    },
+    5: {
+        "question": "A posição correta que o condutor deve adotar para conduzir veículos e:",
+        "options": {
+            "A": "A mão direita sobre a alavanca de câmbio, para facilitar trocas de marchas em situações de emergências.",
+            "B": "Os braços esticados para obter respostas mais rápidas em relação aos próprios reflexos.",
+            "C": "As duas mãos no volante, imitando os ponteiros de um relógio na posição de 9 horas e 15 minutos.",
+            "D": "O pé direito levemente apoiado no freio, para frear mais rapidamente em situações de emergência."
+        },
+        "answer": "C",
+        "difficulty": "Médio"
+    },
+    6: {
+        "status": "Buraco",
+        },
+    7: {
+        "question": "Alfredo dirige, nas proximidades de uma escola municipal, em velocidade incompatível com a segurança no trânsito. Nesse caso, Alfredo será:",
+        "options": {
+            "A": "Multado em dobro, caso a velocidade aferida seja superior em até 20% (vinte por cento) à máxima permitida.",
+            "B": "Multado, caso a velocidade aferida seja superior em até 20% (vinte por cento) à máxima permitida.",
+            "C": "Multado, além de sua conduta ser passível de enquadramento como crime de trânsito.",
+            "D": "Multado, caso a velocidade aferida seja superior a 50% (cinquenta por cento) à máxima permitida."
+        },
+        "answer": "D",
+        "difficulty": "Difícil"
+    },
+    8: {
+        "status": "Buraco"
+    },
+    9: {
+        "question": "Estacionar o veículo ao lado de outro veículo, em fila dupla, terá como penalidade:",
+        "options": {
+            "A": "Multa.",
+            "B": "Suspensão do direito de dirigir.",
+            "C": "Advertência.",
+            "D": "Apreensão do veículo."
+        },
+        "answer": "A",
+        "difficulty": "Difícil"
+    },
+    10: {
+        "question": "Ao atender um motociclista, com alteração de consciência, após ter batido sua cabeça no chão, deve-se:
+",
+        "options": {
+            "A": "Aplicar gelo na cabeça da vítima, após retirar seu capacete.",
+            "B": "Restringir os movimentos da vítima, mantendo suas roupas apertadas.",
+            "C": "Oferecer líquidos à vitima.",
+            "D": "Evitar movimentar a vítima e chamar socorro médico especializado."
+        },
+        "answer": "D",
+        "difficulty": "Muito difícil"
+    },
+    11: {
+        "question": "O condutor que estiver com o direito de dirigir suspenso e for flagrado conduzindo veículo, será punido com:",
+        "options": {
+            "A": "Retenção do veículo.",
+            "B": "Cassação do documento de habilitação.",
+            "C": "Advertência por escrito.",
+            "D": "Remoção do veículo."
+        },
+        "answer": "B",
+        "difficulty": "Muito difícil"
+    },
+    12: {
+        "question": "A conduta adequada e responsável do condutor, caso ingira bebida alcoólica, é:",
+        "options": {
+            "A": "Tomar água reduzindo a concentração de álcool no sangue.",
+            "B": "Não dirigir em hipótese alguma.",
+            "C": "Lavar o rosto com água fria para ficar mais atento.",
+            "D": "Ingerir café para ficar acordado e mais atento."
+        },
+        "answer": "B",
+        "difficulty": "Muito difícil"
+    },
+    13: {
+        "status": "Buraco"
+    },
+    14: {
+        "question": "A educação para o trânsito é:",
+        "options": {
+            "A": "Para quem é motorista",
+            "B": "É direito só dos agentes de trânsito",
+            "C": "É direito só para quem é alfabetizado",
+            "D": "É direito de todos"
+        },
+        "answer": "D",
+        "difficulty": "Médio"
+    },
+    15: {
+        "question": "Quando se está dirigindo, é preciso respeitar as áreas dos pedestres. Qual das  opções abaixo não respeita essas regras?",
+        "options": {
+            "A": "Parar na calçada ou na faixa de pedestres",
+            "B": "Não Parar na calçada ou na faixa de pedestres",
+            "C": "Estacionar em um vaga",
+            "D": "Dirigir nos limites da rua"
+    },
+        "answer": "A",
+        "difficulty": "Fácil"
+    },
+    16: {
+        "question": "No Transito, a segurança é sempre importante, tendo isso em mente qual das  opções abaixo não condiz com essa segurança?",
+        "options": {
+            "A": "Evitar ultrapassagens",
+            "B": "Não dirigir após beber alguma bebida alcoolica",
+            "C": "Não dirigir com Sono",
+            "D": "Não prestar atenção"
+        },
+        "answer": "D",
+        "difficulty": "Fácil"
+    },
+    17: {
+        "status": "Buraco"
+    },
+    18: {
+        "status": "Buraco"
+    },
+    19: {
+        "question": "É preciso ter respeito com as pessoas, e isso também se inclui no transito. Qual  das opções abaixo pode ser considerada uma atitude desrespeitosa?",
+        "options": {
+            "A": "Dirigir nos limites da velocidade",
+            "B": "Estacionar nos locais certos",
+            "C": "Buzinar fora dos padrões",
+            "D": "Utilizar as setas do veiculo"
+        },
+        "answer": "C",
+        "difficulty": "Fácil"
+    },
+    20: {
+        "status": "Buraco"
+   },
+    21: {
+        "question": "Qual das opções abaixo, jamais devem ocorrer no transito.",
+        "options": {
+            "A": "Dar passagem para outro carro",
+            "B": "Jogar água ou sujeira em outros carros ou pessoas.",
+            "C": "Respeitar os sinais de transito.",
+            "D": "Dar passagem a ambulâncias ou carros policiais."
+        },
+        "answer": "B",
+        "difficulty": "Médio"
+   },
+    22: {
+        "question": "Em uma ultrapassagem, é necessário que o carro da frente tenha boa visão de  onde você vai passar. Tendo isso em mente, por onde você deve passar?",
+        "options": {
+            "A": "Por cima do carro, voando",
+            "B": "Por baixo do carro.",
+            "C": "Pela direita, na visão do passageiro",
+            "D": "Pela esquerda, na visão do motorista."
+        },
+        "answer": "D",
+        "difficulty": "Médio"
+    },
+    23: {
+        "status": "Buraco"
+    },
+    24: {
+        "question": "A segurança é uma das partes mais importantes da direção. Qual das opções  abaixo não é considerado algo seguro?",
+        "options": {
+            "A": "Utilizar cinto de segurança",
+            "B": "Ter atenção no transito e em seu redor",
+            "C": "Dirigir com o braço para fora",
+            "D": "Não dirigir utilizando o celular."
+        },
+        "answer": "C",
+        "difficulty": "Médio"
+    },
+    25: {
+        "question": "Qual das opções abaixo pode ser considerada a MAIS perigosa, para se cometer  no transito?",
+        "options": {
+            "A": "Não utilizar cinto de segurança",
+            "B": "Não dar seta ao fazer a curva",
+            "C": "Conversar enquanto dirige",
+            "D": "Utilizar o volante com um mão."
+        },
+        "answer": "A",
+        "difficulty": "Difícil"
+    },
+    26: {
+        "status": "Buraco"
+    },
+    27: {
+        "status": "Buraco"
+    },
+    28: {
+        "question": "Para se manter seguro, também é necessário estar dirigindo algo seguro, então  para isso, é necessário:",
+        "options": {
+            "A": "Dirigir um carro quebrado.",
+            "B": "Dirigir um carro em bom estado de conservação.",
+            "C": "Dirigir com uma das portas abertas.",
+            "D": "Dirigir com pneus furados."
+        },
+        "answer": "B",
+        "difficulty": "Difícil"
+    },
+    29: {
+        "question": "É extremamente importante dirigir com o máximo de atenção possível, por isso,  deve se evitar dirigir após:",
+        "options": {
+            "A": "Beber água.",
+            "B": "Comer Algo.",
+            "C": "Beber algo com álcool.",
+            "D": "Beber Refrigerante."
+        },
+        "answer": "C",
+        "difficulty": "Muito difícil"
+    },
+    30: {
+        "question": "O semáforo é utilizado para controlar o trânsito, ele contem três cores,  vermelho, amarelo e verde. O que essas cores significam?",
+        "options": {
+            "A": "Vermelho: Para parar. Amarelo: Para prestar atenção.  Verde: Significa que pode andar.",
+            "B": "Vermelho: Para andar mais rápido. Amarelo: Significa para manter  uma velocidade. Verde: Significa para parar.",
+            "C": "Vermelho: Para prestar atenção. Amarelo: Significa que pode  andar. Verde: Para parar.",
+            "D": "Vermelho: Significa que pode andar. Amarelo: Para prestar  atenção. Verde: Para parar."
+        },
+        "answer": "A",
+        "difficulty": "Muito difícil"
+    },
+}
